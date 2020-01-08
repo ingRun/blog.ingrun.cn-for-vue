97a79f8634abaf832f3fbe63e1c49b5c41e54043
ingrun <ingrun@163.com>
2020-01-08 23:40:20 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="hello">
-      <p name="1"> 创建blog 的 vue 在 GitHub 上的项目  并同步  </p>
-      <p> 创建脚本 完成 blog 后端 项目的同步发布 </p>
-      <p> 创建脚本 完成 blog 前 后端 的发布脚本 </p>
+    <p name="1">创建blog 的 vue 在 GitHub 上的项目 并同步</p>
+    <p>创建脚本 完成 blog 后端 项目的同步发布</p>
+    <p>创建脚本 完成 blog 前 后端 的发布脚本</p>
+    <el-button type="primary" @click="login">主要按钮</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  methods: {
+    login() {
+      this.$axios({
+        url: "api/login",
+        method: "post",
+        type: "json",
+        headers: {
+          "Content-Type": 'application/json; charset=utf-8', //这里重点
+        },
+        data: JSON.stringify({"email": "123@456.com"})
+      }).then(res => {
+          if (res == ''){
+            res = res.data
+          }
+      });
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    　　devServer: {
+    　　　　port: 8080,
+    　　　　proxy: {
+    　　　　　　'/api': {
+    　　　　　　　　target: 'http://127.0.0.1:8081',// 要跨域的域名
+    　　　　　　　　changeOrigin: true, // 是否开启跨域
+    　　　　　　　　pathRewrite: {
+    　　　　　　　　'^/api': '/api' // 在请求的时候 凡是/api开头的地址都可以跨域
+    　　　　　　　　}
+    　　　　　　},
+    　　　　}
+    　　}
+    };
